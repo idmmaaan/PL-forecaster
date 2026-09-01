ea3a7604b94f45b0126130e2c2fc213861448005
dilin <dilin@softserveinc.com>
2026-09-01 22:56:24 +0200

    Add frontend part

---
+++ apps/web/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { FixturesPage } from './pages';
+
+function App() {
+  return (
+    <div className="App">
+      <FixturesPage />
+    </div>
+  );
+}
+
+export default App;
